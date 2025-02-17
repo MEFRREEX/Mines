0c772985612da4a833a0f26c3c3caf53e854a9cc
Arkadiy <83061703+MEFRREEX@users.noreply.github.com>
2025-02-17 21:35:06 +0200

    fix: sending updated blocks after mine update

--- src/main/java/com/mefrreex/mines/task/UpdateMineTask.java
+++ src/main/java/com/mefrreex/mines/task/UpdateMineTask.java
@@ -1,10 +1,14 @@
 package com.mefrreex.mines.task;
 
+import cn.nukkit.Player;
 import cn.nukkit.level.Level;
 import cn.nukkit.math.Vector3;
 import cn.nukkit.scheduler.AsyncTask;
 import com.mefrreex.mines.mine.Mine;
 import com.mefrreex.mines.utils.Point;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class UpdateMineTask extends AsyncTask {
 
@@ -32,15 +36,20 @@ public class UpdateMineTask extends AsyncTask {
         int minZ = Math.min(point1.getZ(), point2.getZ());
         int maxZ = Math.max(point1.getZ(), point2.getZ());
 
+        Set<Vector3> positions = new HashSet<>();
+
         for (int x = minX; x <= maxX; x++) {
             for (int y = minY; y <= maxY; y++) {
                 for (int z = minZ; z <= maxZ; z++) {
-                    level.setBlock(new Vector3(x, y, z), mine.getBlockWithChance().getBlock());
+                    Vector3 position = new Vector3(x, y, z);
+                    positions.add(position);
+                    level.setBlock(position, mine.getBlockWithChance().getBlock());
                     mine.getCurrentSize().incrementAndGet();
                 }
             }
         }
 
+        level.sendBlocks(level.getPlayers().values().toArray(new Player[0]), positions.toArray(new Vector3[0]));
         mine.setUpdating(false);
     }
 }
